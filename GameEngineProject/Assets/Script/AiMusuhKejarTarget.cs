b5361fa0c53327164595f3866910cd45508ee7fc
Muhammad Efath <efath.hidayat@gmail.com>
2018-02-28 18:50:19 +0700

    tes commit 2

---
+++ GameEngineProject/Assets/Script/AiMusuhKejarTarget.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiMusuhKejarTarget : MonoBehaviour {
+
+	public float Speed;
+	public GameObject TemplePusat;
+	
+	// Update is called once per frame
+	void Update () {
+		AIJalan ();
+	}
+
+	void AIJalan()
+	{
+		//Debug.Log ((Vector3.Distance(TemplePusat.transform.position , this.transform.position)));
+		//return;
+		Vector3 SelisihPosisi =  TemplePusat.transform.position - this.transform.position ;
+		if(Vector3.Distance(TemplePusat.transform.position , this.transform.position) < 5)
+		{
+			return;
+		}
+
+		this.transform.rotation = Quaternion.Slerp (this.transform.rotation,Quaternion.LookRotation(SelisihPosisi),0.1f);
+		this.transform.Translate (0,0,Speed * Time.deltaTime);
+	}
+}
---
+++ GameEngineProject/Assets/Script/AiMusuhKeluar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiMusuhKeluar : MonoBehaviour {
+
+	public GameObject MusuhPrefab;
+	public Transform[] PosisiKeluarMusuh;
+
+	public float WaktuJedaMusuhKeluar = 3f;
+
+
+	public void Update()
+	{
+		StartCoroutine (KeluarMusuh());
+	}
+
+	public IEnumerator KeluarMusuh()
+	{
+		
+		if (WaktuJedaMusuhKeluar >= 0) {
+			WaktuJedaMusuhKeluar -= Time.deltaTime;
+			yield return 0;
+		} else {
+			int IndexPosisi = Random.Range (0,3);
+			Instantiate (MusuhPrefab,PosisiKeluarMusuh[IndexPosisi].position,PosisiKeluarMusuh[IndexPosisi].rotation);
+			WaktuJedaMusuhKeluar = 3;
+		}
+
+	}
+}
