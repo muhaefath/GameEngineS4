35a4c5c36ae2f2878584d18db3144de2938dd826
muhaefath <32285392+muhaefath@users.noreply.github.com>
2018-03-02 18:11:56 +0700

    Week3Update
    TambahAR = FILE BARU DIGANTI

---
+++ GameEngineProject/Assets/Script/Environment/DayNight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour {
+	public float speed;
+	void Update()
+	{
+		if(this.transform.rotation == Quaternion.Euler(190,0,0))
+		{
+			//ManagerBuilding.Instance.MalamDatang = true;
+			//ManagerBuilding.Instance.MenuBuilding [4].SetActive (true);
+			//Time.timeScale = 0;
+			//return;
+		}
+		this.transform.Rotate (speed*Time.deltaTime , 0, 0);
+	}
+
+	public void LanjutPagi()
+	{
+		//Time.timeScale = 1;
+		//ManagerBuilding.Instance.MalamDatang = false;
+		//ManagerBuilding.Instance.MenuBuilding [4].SetActive (false);
+		this.transform.rotation = Quaternion.Euler (0, 0, 0);
+		this.transform.Rotate (speed*Time.deltaTime , 0, 0);
+	}
+}
