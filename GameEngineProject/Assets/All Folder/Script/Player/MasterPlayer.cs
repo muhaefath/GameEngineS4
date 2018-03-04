11f59f057dd0c0f1e73f4a7d9e3030a71f8e1e75
muhaefath <32285392+muhaefath@users.noreply.github.com>
2018-03-04 19:32:24 +0700

    Week3Update1.4
    1. Script Jalan Karakter
    2. Script Nembak Karakter
    3. Bunuh Musuh

---
+++ GameEngineProject/Assets/All Folder/Script/Player/MasterPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterPlayer : MonoBehaviour {
+
+	public static MasterPlayer Instance;
+
+	public float SpeedJalanPlayer;
+	public float SpeedRotasiPlayer;
+	[Space]
+	public GameObject PeluruPrefab;
+	public Transform PosisiPeluruKeluar;
+
+	void Update()
+	{
+		NavigasiJalan ();
+		TembakPeluru ();
+	}
+
+	void NavigasiJalan()
+	{
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.rotation = Quaternion.Euler (0,-90,0);
+			transform.Translate (0,0,SpeedJalanPlayer*Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.W))
+		{
+			transform.rotation = Quaternion.Euler (0,0,0);
+			transform.Translate (0,0,SpeedJalanPlayer*Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.D))
+		{
+			transform.rotation = Quaternion.Euler (0,90,0);
+			transform.Translate (0,0,SpeedJalanPlayer*Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.S))
+		{
+			transform.rotation = Quaternion.Euler (0,-180,0);
+			transform.Translate (0,0,SpeedJalanPlayer*Time.deltaTime);
+		}
+	}
+
+	void TembakPeluru()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Instantiate (PeluruPrefab,PosisiPeluruKeluar.transform.position,PosisiPeluruKeluar.transform.rotation);
+		}
+	}
+}
---
+++ GameEngineProject/Assets/All Folder/Script/Player/PeluruPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeluruPlayer : MonoBehaviour {
+
+	public float SpeedPeluru;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0,0,SpeedPeluru*Time.deltaTime);
+		Destroy (gameObject,1);
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Enemy")
+		{
+			Destroy (col.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
